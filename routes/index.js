7aaa27446fd7aa30bcff0143bf99bbd417d933b8
RahiMedouni <rahimedouni@gmail.com>
2022-02-05 17:33:25 +0100

    sports array

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,41 @@
 var express = require("express");
 var router = express.Router();
 
+const sports = [
+  {
+    name: "Football",
+    clubs: "100",
+    players: "2000",
+  },
+  {
+    name: "Basketball",
+    clubs: "65",
+    players: "800",
+  },
+  {
+    name: "Handball",
+    clubs: "40",
+    players: "550",
+  },
+  {
+    name: "Volleyball",
+    clubs: "20",
+    players: "260",
+  },
+  {
+    name: "Tennis",
+    clubs: "5",
+    players: "50",
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("welcome to express back end project");
 });
 
 router.get("/sportList", function (req, res, next) {
-  res.send("list of sports");
+  res.send(sports);
 });
 
 module.exports = router;
